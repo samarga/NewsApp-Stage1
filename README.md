525e293912591a29cca5a6a4a98d6add7e5090a4
She <Sheila.Amarga@nike.com>
2018-11-29 01:46:38 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# NewsApp-Stage1
---
+++ app/src/main/java/com/example/samarg/newsapp_stage1/NewsLoader.java
@@ -0,0 +1,31 @@
+package com.example.samarg.newsapp_stage1;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import java.util.List;
+
+public class NewsLoader extends AsyncTaskLoader<List<News>> {
+
+    private static String REQUEST_URL =
+            "http://content.guardianapis.com/technology?show-tags=contributor&api-key=test";
+
+    public NewsLoader(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    @Override
+    public List<News> loadInBackground() {
+        if (REQUEST_URL == null) {
+            return null;
+        }
+
+        List<News> newsList = QueryUtils.fetchNewsData(REQUEST_URL);
+        return newsList;
+    }
+}
